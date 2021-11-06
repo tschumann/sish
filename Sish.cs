7c890537db48cb8392dc3c57379e32bb5b928b44
tschumann <schumann.tom@gmail.com>
2021-11-06 16:24:34 +1000

    Initial import.

---
+++ Sish.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+
+namespace sish
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Please specify a three letter company code");
+
+                System.Environment.Exit(1);
+            }
+
+            string code = args[0];
+            string statisticsUrl = $"https://asx.api.markitdigital.com/asx-research/1.0/companies/{code}/key-statistics";
+
+            HttpResponseMessage response = client.GetAsync(statisticsUrl).Result;
+            Console.WriteLine(response.Content.ReadAsStringAsync());
+        }
+    }
+}
