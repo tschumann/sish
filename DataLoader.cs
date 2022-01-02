0d64f3772d3950ad94a645719c183bcd6fee7e3e
tschumann <schumann.tom@gmail.com>
2022-01-03 07:27:58 +1000

    Moved data loading and caching into its own class.

---
+++ DataLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+namespace sish
+{
+    class DataLoader
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private string data { get; }
+
+        public DataLoader(string code)
+        {
+            string dataFileName = $"cache/{code}.json";
+
+            try
+            {
+                data = File.ReadAllText(dataFileName);
+            }
+            catch(Exception ex)
+            {
+                string statisticsUrl = $"https://asx.api.markitdigital.com/asx-research/1.0/companies/{code}/key-statistics";
+
+                HttpResponseMessage response = client.GetAsync(statisticsUrl).Result;
+                data = response.Content.ReadAsStringAsync().Result;
+
+                // TODO: create a logger class and log here
+                Console.WriteLine(data);
+
+                Directory.CreateDirectory("cache");
+                File.WriteAllText(dataFileName, data);
+            }            
+        }
+    }
+}
--- Sish.cs
+++ Sish.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.IO;
-using System.Net.Http;
 
 namespace sish
 {
     class Sish
     {
-        private static readonly HttpClient client = new HttpClient();
         static void Main(string[] args)
         {
             if (args.Length < 2)
@@ -17,15 +14,7 @@ namespace sish
             }
 
             string code = args[1];
-            string statisticsUrl = $"https://asx.api.markitdigital.com/asx-research/1.0/companies/{code}/key-statistics";
-
-            HttpResponseMessage response = client.GetAsync(statisticsUrl).Result;
-            string result = response.Content.ReadAsStringAsync().Result;
-
-            Console.WriteLine(result);
-
-            Directory.CreateDirectory("cache");
-            File.WriteAllText($"cache/{code}.json", result);
+            DataLoader dataLoader = new DataLoader(code);
         }
     }
 }
